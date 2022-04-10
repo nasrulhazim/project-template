37abc234a1070526bfedaaecc3a96e4c6b8e3905
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-04-10 13:06:28 +0800

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,7 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <livewire:user-datatable />
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <livewire:user-datatable />
+            </div>
         </div>
     </div>
 </x-app-layout>
