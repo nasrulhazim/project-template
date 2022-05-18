6e5cefe42cc3277777b2863dda4c484d9adc3881
Nasrul Hazim Bin Mohamad <10341422+nasrulhazim@users.noreply.github.com>
2022-05-18 19:48:33 +0800

    Update Base.php

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Concerns\HasUser;
 use App\Concerns\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,15 +10,24 @@ use OwenIt\Auditing\Auditable as AuditableTrait;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Base extends Model implements AuditableContract, HasMedia
 {
     use HasFactory;
     use HasUuid;
+    use HasUser;
     use InteractsWithMedia;
     use AuditableTrait;
 
     protected $guarded = [
         'id',
     ];
+    
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(130)
+            ->height(130);
+    }
 }
