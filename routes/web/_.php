56c834ec595c2a8606442f9b99dfd53ca8e42f16
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-06-18 16:18:10 +0800

    Reorganise route

---
+++ routes/web/_.php
@@ -0,0 +1,52 @@
+<?php 
+
+
+use App\Http\Controllers\NotificationController;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/users', function () {
+        return view('users.index');
+    })->name('users.index');
+
+    Route::impersonate();
+
+    Route::get('/notifications', NotificationController::class)->name('notifications');
+});
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect(RouteServiceProvider::HOME);
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
---
+++ support/require.php
@@ -0,0 +1,13 @@
+<?php 
+
+if(! function_exists('require_all_in')) {
+    function require_all_in(string $path)
+    {
+        collect(glob($path))
+            ->each(function ($path) {
+                if (basename($path) !== basename(__FILE__)) {
+                    require $path;
+                }
+            });
+    }
+}
