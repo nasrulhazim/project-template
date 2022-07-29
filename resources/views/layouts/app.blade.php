6f4981595e06c2e98ff7e20ff9ba99b512a9fb1b
Shift <shift@laravelshift.com>
2022-07-29 10:07:46 +0000

    Use new `@vite` Blade directive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @vite('resources/css/app.css')
 
         @livewireStyles
 
         @stack('styles')
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased bg-slate-100">
         <div class="fixed top-0 inset-x-0 z-20">
