5806c795ca39e6304cae2d71e917ca799116eb61
Nasrul Hazim Bin Mohamad <10341422+nasrulhazim@users.noreply.github.com>
2022-08-01 09:44:38 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,16 @@
+const path = require('node:path');
+
 module.exports = {
+    resolve: {
+        alias: {
+            'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js'),
+        },
+    },
+    optimizeDeps: {
+        include: [
+            path.resolve(__dirname, 'tailwind.config.js'),
+        ]
+    },
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
