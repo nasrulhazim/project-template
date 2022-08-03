b26f52f95518940fca4eb5de7d310753bdae3826
Nasrul Hazim Bin Mohamad <10341422+nasrulhazim@users.noreply.github.com>
2022-08-03 21:43:55 +0800

    Update AbstractAction.php

--- app/Actions/AbstractAction.php
+++ app/Actions/AbstractAction.php
@@ -3,6 +3,8 @@
 namespace App\Actions;
 
 use App\Contracts\Execute;
+use App\Exceptions\ActionException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 abstract class AbstractAction implements Execute
@@ -18,11 +20,25 @@ abstract class AbstractAction implements Execute
         return $this->input;
     }
 
+    public function model(): string
+    {
+        if (! property_exists($this, 'model')) {
+            throw ActionException::missingModelProperty(__CLASS__);
+        }
+
+        return $this->model;
+    }
+
     public function execute()
     {
         Validator::make(
             $this->inputs(),
             $this->rules()
         )->validate();
+
+
+        return DB::transaction(function () {
+            return $this->model()::create($this->inputs());
+        });
     }
 }
