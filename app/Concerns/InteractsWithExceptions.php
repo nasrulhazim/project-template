2aca1494738479a44d42b272413574a7f7365494
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-11 16:28:16 +0800

    Added Interacts with Exception Handler

---
+++ app/Concerns/InteractsWithExceptions.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Concerns;
+
+trait InteractsWithExceptions
+{
+    public static function throwIf(bool $condition, string $method, ...$args)
+    {
+        if (! $condition) {
+            if (method_exists(__CLASS__, $method)) {
+                throw self::$method(...$args);
+            }
+
+            throw new self("Invalid exception method $method");
+        }
+    }
+
+    public static function throwUnless(bool $condition, string $method, ...$args)
+    {
+        self::throwIf(! $condition, $method, ...$args);
+    }
+}
