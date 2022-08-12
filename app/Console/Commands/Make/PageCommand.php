447b1a3fd43b4c82d39329eeaa8dc619f390098f
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-12 23:22:53 +0800

    Added make:page command

---
+++ app/Console/Commands/Make/PageCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands\Make;
+
+use Illuminate\Console\Command;
+
+class PageCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:page {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Prepare common classes for a page';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('make:model', [
+            'name' => $this->argument('name'),
+            '--controller' => true,
+            '--factory' => true,
+            '--migration' => true,
+            '--resource' => true,
+            '--pest' => true,
+            '--seed' => true,
+        ]);
+
+        $this->call('make:livewire', [
+            'name' => $this->argument('name') . 'Form',
+        ]);
+
+        $this->call('make:datatable', [
+            'name' => $this->argument('name') . 'Datatable',
+            'model' => $this->argument('name'),
+        ]);
+        $this->call('make:route', [
+            'name' => $this->argument('name'),
+        ]);
+
+        return 0;
+    }
+}
