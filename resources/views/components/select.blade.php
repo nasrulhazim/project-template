8e1302b5d5a4e226470f8bedbba7fe6a029e06b9
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-13 01:00:15 +0800

    Added <x-select /> component

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,14 @@
+
+@props(['id' => null, 'key' => null, 'model' => null, 'options' => []])
+
+<select
+    wire:model.stop="{{ $model }}"
+    wire:key="{{ $key }}"
+    id="{{ $id }}"
+    class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600"
+>
+    <option>{{ __('Please select') }}</option>
+    @foreach($options as $key => $value)
+        <option value="{{ $key }}">{{ $value }}</option>
+    @endforeach
+</select>
