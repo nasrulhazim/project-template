f1e4b5cb9f08f0dea7798b07a2ec9ec9c4c1136d
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-14 23:09:29 +0800

    fixed logo alignment

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex-shrink-0 flex items-center px-4">
-    <a href="{{ auth()->user() ? route('dashboard') : url('/') }}" class="text-center w-full font-bold">
+    <a href="{{ auth()->user() ? route('dashboard') : url('/') }}" class="flex font-bold justify-center text-center w-full">
         @if (file_exists(public_path('storage/logo.png')))
             <img class="h-8 w-auto" src="{{ url('storage/logo.png') }}" alt="{{ config('app.name') }}">
         @else
