5edfad892079e7e425fc94648797184016b8a8b6
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-16 20:53:45 +0800

    Update datatable action

--- resources/views/livewire/datatable-actions.blade.php
+++ resources/views/livewire/datatable-actions.blade.php
@@ -2,16 +2,20 @@
 
     @isset($view)
         <a class="cursor-pointer mr-4" href="{{ $view }}">
-            {{ __('View') }}
+            <x-icon name="o-eye" class="text-indigo hover:font-bold mr-3 flex-shrink-0 h-6 w-6">
+            </x-icon>
         </a>
     @endisset
 
     <div class="cursor-pointer mr-4" wire:click="$emitTo('{{ $form }}', 'showRecord', '{{ $row->uuid }}')">
-        {{ __('Edit') }}
+        <x-icon name="o-pencil" class="text-indigo hover:font-bold mr-3 flex-shrink-0 h-6 w-6">
+        </x-icon>
     </div>
 
-    <div class="cursor-pointer" class="bg-red-500"
+    <div class="cursor-pointer" 
         wire:click="$emitTo('confirm', 'displayConfirmation', 'Delete Record', 'Are you sure?', '{{ $form }}', 'destroyRecord', '{{ $row->uuid }}')">
-        {{ __('Remove') }}
+        <x-icon name="o-trash"
+            class="text-indigo hover:text-red-500 mr-3 flex-shrink-0 h-6 w-6">
+        </x-icon>
     </div>
 </div>
