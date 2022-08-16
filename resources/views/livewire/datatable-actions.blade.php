3ec44dee2565aa252c5d87798dbc3d71a1352246
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-16 20:47:34 +0800

    Added view action button

--- resources/views/livewire/datatable-actions.blade.php
+++ resources/views/livewire/datatable-actions.blade.php
@@ -1,9 +1,16 @@
 <div class="flex justify-items-center">
+
+    @isset($view)
+        <a class="cursor-pointer mr-4" href="{{ $view }}">
+            {{ __('View') }}
+        </a>
+    @endisset
+
     <div class="cursor-pointer mr-4" wire:click="$emitTo('{{ $form }}', 'showRecord', '{{ $row->uuid }}')">
         {{ __('Edit') }}
     </div>
 
-    <div class="cursor-pointer" class="bg-red-500" 
+    <div class="cursor-pointer" class="bg-red-500"
         wire:click="$emitTo('confirm', 'displayConfirmation', 'Delete Record', 'Are you sure?', '{{ $form }}', 'destroyRecord', '{{ $row->uuid }}')">
         {{ __('Remove') }}
     </div>
