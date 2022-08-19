8cae7500fcfad3fa6a5ba95eb27b6965f8be4804
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-19 09:10:09 +0800

    Make select component defer

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,8 +2,7 @@
 @props(['id' => null, 'key' => null, 'model' => null, 'options' => []])
 
 <select
-    wire:model.stop="{{ $model }}"
-    wire:key="{{ $key }}"
+    wire:model.defer="{{ $model }}"
     id="{{ $id }}"
     class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600"
 >
