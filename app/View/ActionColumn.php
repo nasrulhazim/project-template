daa2eec7735d0a1c4eb4486f40598c5756caca7c
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-20 23:58:19 +0800

    Added ActionColumn

---
+++ app/View/ActionColumn.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View;
+
+use Illuminate\Database\Eloquent\Model;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+
+class ActionColumn extends Column
+{
+    protected string $form;
+
+    public function form(string $form): self
+    {
+        $this->form = $form;
+
+        return $this;
+    }
+
+    public function getView(): string 
+    {
+        return property_exists($this, 'view') ? $this->view : 'livewire.datatable-actions';
+    }
+
+    public function getContents(Model $row)
+    {
+        return view($this->getView(), ['form' => $this->form])
+            ->withColumn($this)
+            ->withRow($row);
+    }
+}
