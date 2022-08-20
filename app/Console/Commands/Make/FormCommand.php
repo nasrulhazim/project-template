f5391b430186df412453bbb9c412c34865499575
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-21 01:12:19 +0800

    Added make:form command

---
+++ app/Console/Commands/Make/FormCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands\Make;
+
+use Illuminate\Console\Command;
+
+class FormCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:form {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Livewire Form';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('make:livewire', [
+            'name' => 'Forms\\' . $this->argument('name'),
+        ]);
+        
+        $this->comment('See ' . base_path('stubs/livewire.form.stub') . ' for additional setup for the class');
+        $this->comment('See ' . base_path('stubs/livewire.form.view.stub') . ' for additional setup for the view');
+        return 0;
+    }
+}
