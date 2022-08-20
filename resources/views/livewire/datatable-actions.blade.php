77aec7e62e56274ba741f647752d7c57e576b3b8
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-20 23:58:45 +0800

    Added datatable action view

--- resources/views/livewire/datatable-actions.blade.php
+++ resources/views/livewire/datatable-actions.blade.php
@@ -1,21 +1,26 @@
 <div class="flex justify-items-center">
 
     @isset($view)
-        <a class="cursor-pointer mr-4" href="{{ $view }}">
-            <x-icon name="o-eye" class="text-indigo hover:font-bold mr-3 flex-shrink-0 h-6 w-6">
-            </x-icon>
-        </a>
+        @can('view', $row)
+            <a class="cursor-pointer mr-4" href="{{ $view }}">
+                <x-icon name="o-eye" class="text-indigo hover:font-bold mr-3 flex-shrink-0 h-6 w-6">
+                </x-icon>
+            </a>
+        @endcan
     @endisset
 
-    <div class="cursor-pointer mr-4" wire:click="$emitTo('{{ $form }}', 'showRecord', '{{ $row->uuid }}')">
-        <x-icon name="o-pencil" class="text-indigo hover:font-bold mr-3 flex-shrink-0 h-6 w-6">
-        </x-icon>
-    </div>
+    @can('update', $row)
+        <div class="cursor-pointer mr-4" wire:click="$emitTo('{{ $form }}', 'showRecord', '{{ $row->uuid }}')">
+            <x-icon name="o-pencil" class="text-indigo hover:font-bold mr-3 flex-shrink-0 h-6 w-6">
+            </x-icon>
+        </div>
+    @endcan
 
-    <div class="cursor-pointer" 
-        wire:click="$emitTo('confirm', 'displayConfirmation', 'Delete Record', 'Are you sure?', '{{ $form }}', 'destroyRecord', '{{ $row->uuid }}')">
-        <x-icon name="o-trash"
-            class="text-indigo hover:text-red-500 mr-3 flex-shrink-0 h-6 w-6">
-        </x-icon>
-    </div>
+    @can('delete', $row)
+        <div class="cursor-pointer"
+            wire:click="$emitTo('confirm', 'displayConfirmation', 'Delete Record', 'Are you sure?', '{{ $form }}', 'destroyRecord', '{{ $row->uuid }}')">
+            <x-icon name="o-trash" class="text-indigo hover:text-red-500 mr-3 flex-shrink-0 h-6 w-6">
+            </x-icon>
+        </div>
+    @endcan
 </div>
