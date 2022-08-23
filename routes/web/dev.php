a4db76908bcd39359fc0a2a37fec824e2c58f6c0
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-23 17:32:20 +0800

    Added doc/db-schema route for database schema view on web

---
+++ routes/web/dev.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+if(app()->environment() != 'production') {
+    Route::get('doc/db-schema', function() {
+
+        $filename = config('app.name') . ' Database Schema.md';
+        $filepath = config('database.doc_schema_path') . DIRECTORY_SEPARATOR . $filename;
+        
+        abort_if(! file_exists($filepath), 404, 'Database schema document not yet generated. Do run php artisan db:schema to generate the schema document.');
+
+        return view('markdown', [
+            'content' => Str::markdown(
+                file_get_contents(
+                    $filepath
+                )
+            ),
+        ]);
+    });
+}
