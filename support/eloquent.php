5a24cbf02dda918880b2437215ec851bfc9df83c
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-09-01 16:42:39 +0800

    Added get_model_from_uuid

---
+++ support/eloquent.php
@@ -0,0 +1,15 @@
+<?php 
+
+use App\Exceptions\ThrowException;
+use Illuminate\Support\Facades\Cache;
+
+if (! function_exists('get_model_from_uuid')) {
+    function get_model_from_uuid(string $uuid, string $class) {
+        ThrowException::unless(class_exists($class), null, "$class did not exists");
+
+        $key = str($class)->kebab()->toString() . '.' . $uuid;
+        
+        return Cache::remember($key, now()->addMinute(), fn() => $class::whereUuid($uuid)->firstOrFail() );
+    }
+}
+    
--- support/uuid-id.php
+++ support/uuid-id.php
@@ -5,7 +5,7 @@ use App\Exceptions\ThrowException;
 if (! function_exists('uuid2id')) {
     function uuid2id(string $uuid, string $class)
     {
-        ThrowException::if(class_exists($class), null, "$class did not exists");
+        ThrowException::unless(class_exists($class), null, "$class did not exists");
 
         $object = $class::whereUuid($uuid)->firstOrFail();
 
@@ -16,7 +16,7 @@ if (! function_exists('uuid2id')) {
 if (! function_exists('id2uuid')) {
     function id2uuid(int $id, string $class)
     {
-        ThrowException::if(class_exists($class), null, "$class did not exists");
+        ThrowException::unless(class_exists($class), null, "$class did not exists");
 
         $object = $class::whereId($id)->firstOrFail();
 
