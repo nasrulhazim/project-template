2136e34a46c1269a394ff6236d1a6a31a6b84ea7
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-09-17 23:32:51 +0800

    Update select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,13 +1,5 @@
-
-@props(['id' => null, 'key' => null, 'model' => null, 'options' => []])
-
-<select
-    wire:model.defer="{{ $model }}"
-    id="{{ $id }}"
-    class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600"
->
+<select {{ $attributes }}
+    class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600">
     <option>{{ __('Please select') }}</option>
-    @foreach($options as $key => $value)
-        <option value="{{ $key }}">{{ $value }}</option>
-    @endforeach
+    {{ $slot }}
 </select>
