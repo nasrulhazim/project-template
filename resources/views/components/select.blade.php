a1f2b24a77e764bde437f6be036ffdb9433dd695
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-09-20 05:23:14 +0800

    Update select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,5 +1,4 @@
-<select {{ $attributes }}
-    class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600">
+<select {{ $attributes->merge(['class' => 'block w-full rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600']) }}>
     <option>{{ __('Please select') }}</option>
     {{ $slot }}
 </select>
