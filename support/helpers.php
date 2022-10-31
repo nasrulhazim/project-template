fadfb7934c879b024c95dba618f6d236a576c690
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-10-31 11:46:37 +0800

    Update

--- support/helpers.php
+++ support/helpers.php
@@ -1,3 +1,15 @@
 <?php
 
+if (! function_exists('require_all_in')) {
+    function require_all_in(string $path)
+    {
+        collect(glob($path))
+            ->each(function ($path) {
+                if (basename($path) !== basename(__FILE__)) {
+                    require $path;
+                }
+            });
+    }
+}
+
 require_all_in(__DIR__.'/*.php');
