46625ca842aa178e0d659eea258a11b0ae201384
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-01-17 20:28:36 +0800

    Update Team Trait

--- app/Concerns/InteractsWithTeam.php
+++ app/Concerns/InteractsWithTeam.php
@@ -4,9 +4,19 @@ namespace App\Concerns;
 
 use App\Models\Team;
 use App\Scopes\Team as TeamScope;
+use Illuminate\Support\Facades\Schema;
 
 trait InteractsWithTeam
 {
+    public static function bootInteractsWithTeam()
+    {
+        static::creating(function ($model) {
+            if (Schema::hasColumn($model->getTable(), 'team_id') && is_null($model->team_id)) {
+                $model->team_id = auth()->user()->currentTeam->id;
+            }
+        });
+    }
+
     /**
      * The "booted" method of the model.
      *
@@ -17,11 +27,6 @@ trait InteractsWithTeam
         parent::booted();
 
         static::addGlobalScope(new TeamScope());
-    }
-
-    public static function bootInteractsWithTeam()
-    {
-        // do nothing atm
     }
 
     public function team()
