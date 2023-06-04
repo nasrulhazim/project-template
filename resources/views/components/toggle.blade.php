091819ee55d4088616345c0867a25e722925b03e
Nasrul Hazim Bin Mohamad <10341422+nasrulhazim@users.noreply.github.com>
2023-06-04 12:14:16 +0800

    Create toggle.blade.php

---
+++ resources/views/components/toggle.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <div x-data="{ toggle : @entangle($status).defer }" class="my-2">
+        <x-jet-label class="mb-1">{{ $label ?? 'Status' }}</x-jet-label>
+        <button type="button"
+             x-on:click="toggle = ! toggle"
+            x-bind:class="toggle ? 'bg-indigo-600' : 'bg-gray-200' "
+            class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+            role="switch" aria-checked="false">
+            <span class="sr-only">{{ $label ?? 'Status' }}</span>
+            <span aria-hidden="true"
+                x-bind:class="toggle ? 'translate-x-5' : 'translate-x-0'"
+                class="pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"></span>
+        </button>
+    </div>
+</div>
