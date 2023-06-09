70cc45455a0a6b110134bb45f9c1e1a47ef647ff
Nasrul Hazim Bin Mohamad <10341422+nasrulhazim@users.noreply.github.com>
2023-06-09 15:50:29 +0800

    Create trash.blade.php

---
+++ resources/views/components/trash.blade.php
@@ -0,0 +1,5 @@
+<div {{ $attributes->merge(['class' => 'border hover:border-red-700 rounded cursor-pointer hover:text-red-700']) }}>
+    <x-icon name="o-trash"
+        class="text-slate-900 mx-2 my-1 flex-shrink-0 h-4 w-4 hover:text-red-700">
+    </x-icon>
+</div>
