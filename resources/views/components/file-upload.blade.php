14d7b1149113b553e6f2f0d78750d41ab1534a4d
Nasrul Hazim Bin Mohamad <10341422+nasrulhazim@users.noreply.github.com>
2023-06-09 15:51:48 +0800

    Create file-upload.blade.php

---
+++ resources/views/components/file-upload.blade.php
@@ -0,0 +1,18 @@
+@props([
+    'name' => 'file',
+    'label' => 'File',
+    'accept' => false,
+    'multiple' => false,
+    'required' => false,
+])
+
+<div>
+    <input wire:model.defer="{{ $name }}"
+        class="bg-gray-50 block border border-gray-300 cursor-pointer dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 focus:outline-none text-gray-900 w-full rounded"
+        type="file"
+        @if($accept) accept="{{ $accept }}" @endif
+        @if($multiple) multiple @endif
+        @if($required) required @endif
+    >
+    <x-jet-input-error for="{{ $name }}" class="mt-2" />
+</div>
