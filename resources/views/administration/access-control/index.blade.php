0ce67243c750afb3435cfaac1a350a74cf82ea36
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-02-12 20:22:57 +0800

    Change <livewire /> to @livewire()

--- resources/views/administration/access-control/index.blade.php
+++ resources/views/administration/access-control/index.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <livewire:datatable.access-control />
+            @livewire('datatable.access-control')
         </div>
     </div>
 </x-app-layout>
--- resources/views/administration/users/index.blade.php
+++ resources/views/administration/users/index.blade.php
@@ -8,7 +8,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <livewire:datatable.user />
+            @livewire('datatable.user')
         </div>
     </div>
 </x-app-layout>
