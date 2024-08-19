33e71889d9c73a58a4a77f49fc52a395cb5eb5cc
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-08-19 22:22:17 +0800

    Fix vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'app/',
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
