3f8e2d3868e3ebfc5360eff38a0c5af1e6e87f58
Nasrul Hazim <nasrulhazim.m@gmail.com>
2024-10-03 15:37:06 +0800

    Create Searchable.php

---
+++ app/Concerns/Scopes/Searchable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Concerns\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
+
+trait Searchable
+{
+    /**
+     * Apply a case-insensitive search to a query builder.
+     *
+     * @param  string|array  $field
+     * @param  string  $keyword
+     * @return Builder
+     */
+    public static function scopeSearch(Builder $query, string|array $fields, $keyword)
+    {
+        $keyword = strtolower($keyword);
+
+        if (is_string($fields)) {
+            $fields = Arr::wrap($fields);
+        }
+
+        foreach ($fields as $field) {
+            $field = '"'.str_replace('.', '"."', $field).'"';
+            $query->orWhereRaw('LOWER('.$field.') LIKE ?', ['%'.$keyword.'%']);
+        }
+
+        return $query;
+    }
+}
