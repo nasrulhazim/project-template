dcbbfe8502101c72aa02479fbfc9eecd35b4cb09
nasrulhazim <nasrulhazim@users.noreply.github.com>
2024-12-09 13:43:33 +0000

    PHP Linting (Pint)

--- rector.php
+++ rector.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
 use Rector\Config\RectorConfig;
-use RectorLaravel\Set\LaravelLevelSetList;
 use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-        __DIR__ . '/support',
-        __DIR__ . '/tests',
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/support',
+        __DIR__.'/tests',
     ]);
 
     $rectorConfig->skip([
