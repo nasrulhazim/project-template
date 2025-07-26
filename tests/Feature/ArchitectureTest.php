3e0f5e10b2e56008c953958c24b07b19d34b1684
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2025-07-26 10:34:43 +0800

    Update Architecture Test

--- tests/Feature/ArchitectureTest.php
+++ tests/Feature/ArchitectureTest.php
@@ -1,19 +1,77 @@
 <?php
 
-it('runs on PHP 8.3 or above')
+it('runs on PHP 8.4 or above')
     ->expect(phpversion())
-    ->toBeGreaterThanOrEqual('8.3.0');
+    ->toBeGreaterThanOrEqual('8.4.0');
 
-it('does not use debugging functions')
-    ->expect(['dd', 'dump', 'ray', 'var_dump', 'var_export'])
+arch()
+    ->expect(['dd', 'dump', 'ray'])
     ->not
-    ->toBeUsed();
+    ->toBeUsedIn([
+        'app',
+        'config',
+        'database',
+        'routes',
+        'support',
+    ]);
 
 it('does not using url method')
     ->expect(['url'])
     ->not
     ->toBeUsed();
 
-test('controllers')
+// Controllers must end with Controller
+arch()
     ->expect('App\Http\Controllers')
     ->toHaveSuffix('Controller');
+
+// Policies must be in App\Policies
+arch()
+    ->expect('App\Policies')
+    ->toBeClasses();
+
+arch()
+    ->expect('App\Policies')
+    ->toHaveSuffix('Policy');
+
+// Mails must be in App\Mail
+arch()
+    ->expect('App\Mail')
+    ->toBeClasses();
+
+arch()
+    ->expect('App\Mail')
+    ->toExtend('Illuminate\Mail\Mailable');
+
+arch()
+    ->expect('env')
+    ->toOnlyBeUsedIn([
+        'config',
+    ]);
+
+arch()
+    ->expect('App')
+    ->not
+    ->toUse([
+        'DB::raw',
+        'DB::select',
+        'DB::statement',
+        'DB::table',
+        'DB::raw',
+        'DB::select',
+        'DB::insert',
+        'DB::update',
+        'DB::delete',
+    ]);
+
+arch()
+    ->expect('App\Concerns')
+    ->toBeTraits();
+
+arch()
+    ->expect('App\Enums')
+    ->toBeEnums();
+
+arch()
+    ->expect('App\Contracts')
+    ->toBeInterfaces();
